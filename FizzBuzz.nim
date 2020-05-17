3d5047331f302c2eb2f7705b29cdfe9cdd46751e
Axel Anaya <axel_anaya@hotmail.com>
2020-05-17 12:59:26 -0500

    Comments added

--- FizzBuzz.nim
+++ FizzBuzz.nim
@@ -1,3 +1,13 @@
+# Classic FizzBuzz exercise
+
+#[
+    print all number from 0 to n, but instead of saying
+    numbers divisible by 3, say "Fizz".
+    And instead of saying numbers
+    divisible by 5, say "Buzz". For
+    numbers divisible by both (3 and 5), say "FizzBuzz".
+]# 
+
 from strutils import parseInt
 
 echo "Give me a limit"
@@ -9,10 +19,10 @@ for i in 0..num:
 
     result = ""
     if i mod 3 == 0:
-        result = result & "Fizz"
+        result &= "Fizz"
     
     if i mod 5 == 0:
-        result = result & "Buzz"
+        result &= "Buzz"
 
     if result.len == 0:
         result = $i
