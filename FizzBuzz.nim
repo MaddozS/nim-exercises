7342ea57dc2b8753c3f91bf1d26ded8520cdf797
Axel Anaya <axel_anaya@hotmail.com>
2020-05-17 00:23:34 -0500

    FizzBuzz exercise

---
+++ FizzBuzz.nim
@@ -0,0 +1,20 @@
+from strutils import parseInt
+
+echo "Give me a limit"
+let num = parseInt(readLine(stdin))
+
+var result:string
+
+for i in countup(0,num):
+    
+    result = ""
+    if i mod 3 == 0:
+        result = result & "Fizz"
+    
+    if i mod 5 == 0:
+        result = result & "Buzz"
+
+    if result.len == 0:
+        result = $i
+    
+    echo result
